84b68dc39309ce4a395ee0978edb5479e257c92f
DJacobson17 <83249575+DJacobson17@users.noreply.github.com>
2023-09-14 16:07:47 -0500

    styled game board

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const Gameboard = (() => {
-  const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  let board = [];
+  const grid = document.getElementById("game_board");
+  
+  const init = function () {
+    let counter = 0;
+    for (let i = 0; i < 9; i++) {
+      const space = document.createElement('div');
+      const img = document.createElement('img');
+      space.appendChild(img);
+      space.id = `grid-${counter}`;
+      // space.dataset.player = '';
+      counter++;
+      board.push(space);
+      grid.appendChild(space);
+    }
+  };
+    return {
+      grid: grid,
+      board,
+      init
+    };
 
+  })();
 
-})();
+Game = Gameboard.init();
+
